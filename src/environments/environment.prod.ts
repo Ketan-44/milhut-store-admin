f98212c3e101a96571a20a3687bfd6c0ba0798d5
keval trivedi <keval.trivedi@thetatechnolabs.com>
2026-06-22 15:42:43 +0530

    feat: updated env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'http://192.168.29.22:3000/api'
+  apiUrl: 'https://milhut-store-api-production.up.railway.app/api'
 };
