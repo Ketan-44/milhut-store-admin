c590765ad531ed09ca31051ea102cf76d0ed4dd1
keval trivedi <kevaltrivedi7600@gmail.com>
2026-08-03 22:28:05 +0530

    feat: updated env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'https://milhut-store-api-production.up.railway.app/api'
+  apiUrl: 'https://milhut-store-api.vercel.app/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   appVersion: packageInfo.version,
   production: false,
   // apiUrl: 'http://192.168.29.22:3000/api'
-  apiUrl: 'https://milhut-store-api-production.up.railway.app/api'
+  apiUrl: 'https://milhut-store-api.vercel.app/api'
 };
 
 /*
